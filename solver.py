9946ccef221713c6a9af7d49c132c33b23f36cb3
Benedikt Werner <1benediktwerner@gmail.com>
2019-01-27 06:11:13 +0100

    Refactoring

--- solver.py
+++ solver.py
@@ -1,11 +1,12 @@
+import time
+import math
+
 import cv2
 import numpy as np
-import math
 import mss
 import pytesseract
-import time
-import win32api
 import win32con
+import win32api
 import win32gui
 from z3 import Bool, If, simplify, sat, Solver
 
@@ -15,34 +16,44 @@ BORDER_LEFT = 100
 BORDER_BOTTOM = 100
 BORDER_RIGHT = 100
 CELL_SIZE = 35
-NUMBER_ONE_WIDTH = 11
-
-WINDOW_HANDLE = win32gui.FindWindow(None, "CrossCells")
-LEFT, TOP, RIGHT, BOTTOM = win32gui.GetWindowRect(WINDOW_HANDLE)
-MONITOR = {
-    "left": LEFT + BORDER_LEFT,
-    "top": TOP + BORDER_TOP,
-    "width": RIGHT - LEFT - BORDER_LEFT - BORDER_RIGHT,
-    "height": BOTTOM - TOP - BORDER_TOP - BORDER_BOTTOM
-}
 
 
 class Rect:
     def __init__(self, rect):
-        self.x = rect[0]
-        self.y = rect[1]
-        self.width = rect[2]
-        self.height = rect[3]
+        if isinstance(rect, Rect):
+            self.x = rect.x
+            self.y = rect.y
+            self.width = rect.width
+            self.height = rect.height
+        else:
+            self.x = rect[0]
+            self.y = rect[1]
+            self.width = rect[2]
+            self.height = rect[3]
         self.x2 = self.x + self.width
         self.y2 = self.y + self.height
         self.center_x = self.x + self.width // 2
         self.center_y = self.y + self.height // 2
 
-    def to_rect(self, border=0):
-        return (self.x-border, self.y-border, self.width+border*2, self.height+border*2)
+    @staticmethod
+    def from_corner_rect(rect):
+        x1, y1, x2, y2 = rect
+        return Rect((x1, y1, x2-x1, y2-y1))
+
+    def enlarge(self, border):
+        return Rect((self.x-border, self.y-border, self.width+border*2, self.height+border*2))
+
+    def to_rect(self):
+        return (self.x, self.y, self.width, self.height)
+
+    def to_slice(self):
+        return (slice(self.y, self.y2), slice(self.x, self.x2))
 
     def dist(self, other):
         return math.sqrt((self.center_x - other.center_x)**2 + (self.center_y - other.center_y)**2)
+
+    def contains(self, other):
+        return self.x < other.x and other.x2 < self.x2 and self.y < other.y and other.y2 < self.y2
 
 
 class Label(Rect):
@@ -80,10 +91,10 @@ class Cell(Rect):
         self.variable = Bool("{},{}".format(self.x, self.y))
 
 
-class Square(Rect):
-    def __init__(self, rect):
+class Constraint(Rect):
+    def __init__(self, rect, initial_cells=None):
         super().__init__(rect)
-        self.cells = []
+        self.cells = [] if initial_cells is None else initial_cells
         self.text = None
 
     def get_constraint(self):
@@ -99,197 +110,228 @@ class Square(Rect):
         return term == int(self.text)
 
 
-class Line:
+class LineConstraint(Constraint):
     def __init__(self, rect, cell=None):
-        self.x = rect[0] + rect[2] // 2
-        self.y = rect[1] + rect[3] // 2
-        self.center_x = self.x
-        self.center_y = self.y
-        self.cells = [] if cell is None else [cell]
-        self.horizontal = None
-        self.text = None
+        super().__init__(rect, None if cell is None else [cell])
 
-    def order_cells(self):
+    def sort_cells(self):
         if max(abs(cell.center_x - self.x) for cell in self.cells) < CELL_SIZE:
             self.cells.sort(key=lambda c: c.y, reverse=self.cells[0].y > self.y)
-            self.horizontal = False
         else:
             self.cells.sort(key=lambda c: c.x, reverse=self.cells[0].x > self.x)
-            self.horizontal = True
 
-    def dist(self, other):
-        return math.sqrt((self.center_x - other.center_x)**2 + (self.center_y - other.center_y)**2)
 
-    def get_constraint(self):
-        if "[" in self.text:
-            return sum(If(cell.variable, 1, 0) for cell in self.cells) == int(self.text.strip("[]"))
+def find_bounding_rects(img, inside=False):
+    mode = cv2.RETR_LIST if inside else cv2.RETR_EXTERNAL
+    contours, _ = cv2.findContours(img, mode, cv2.CHAIN_APPROX_SIMPLE)
+    return [Rect(cv2.boundingRect(contour)) for contour in contours]
 
-        term = 0
+
+class CrossCellsSolver:
+    def __init__(self):
+        self.window_handle = win32gui.FindWindow(None, "CrossCells")
+        self.window_rect = Rect.from_corner_rect(win32gui.GetWindowRect(self.window_handle))
+        self.monitor = {
+            "left": self.window_rect.x + BORDER_LEFT,
+            "top": self.window_rect.y + BORDER_TOP,
+            "width": self.window_rect.width - BORDER_LEFT - BORDER_RIGHT,
+            "height": self.window_rect.height - BORDER_TOP - BORDER_BOTTOM
+        }
+        self.img = None
+        self.img_orig = None
+        self.cells = []
+        self.squares = []
+        self.lines = []
+        self.labels = []
+
+    def move_mouse(self, x=0, y=0):
+        win32api.SetCursorPos((
+            x + self.window_rect.x + BORDER_LEFT,
+            y + self.window_rect.y + BORDER_TOP
+        ))
+
+    def click(self, x, y, right=False):
+        self.move_mouse(x, y)
+        time.sleep(0.01)
+
+        x = x + self.window_rect.x + BORDER_LEFT
+        y = y + self.window_rect.y + BORDER_TOP
+
+        btn_down = win32con.MOUSEEVENTF_RIGHTDOWN if right else win32con.MOUSEEVENTF_LEFTDOWN
+        btn_up = win32con.MOUSEEVENTF_RIGHTUP if right else win32con.MOUSEEVENTF_LEFTUP
+
+        win32api.mouse_event(btn_down, x, y, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(btn_up, x, y, 0, 0)
+        time.sleep(0.01)
+
+    def screenshot(self):
+        with mss.mss() as sct:
+            return np.array(sct.grab(self.monitor))
+
+    def detect_text(self, rect):
+        return pytesseract.image_to_string(self.img[rect.to_slice()], config="tesseract.conf")
+
+    def draw_line(self, start, end, color=(0, 0, 255)):
+        self.img_orig = cv2.line(self.img_orig, start, end, color)
+
+    def draw_rect(self, rect, color=(0, 0, 255)):
+        self.img_orig = cv2.rectangle(self.img_orig, rect.to_rect(), color)
+
+    def draw_text(self, text, rect, color=(0, 0, 255)):
+        self.img_orig = cv2.putText(self.img_orig, text, (rect.x, rect.y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+    def do_level(self):
+        self.detect_level()
+        self.solve_level()
+
+    def detect_level(self):
+        self.img = self.screenshot()
+        self.img_orig = self.img.copy()
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        _, self.img = cv2.threshold(self.img, 110, 255, cv2.THRESH_BINARY_INV)
+
+        self.cells = []
+        self.squares = []
+        self.lines = []
+        self.labels = []
+
+        self.detect_objects()
+        self.process_labels()
+        self.process_cells()
+        self.process_lines()
+        self.process_squares()
+
+        if self.labels:
+            for label in self.labels:
+                self.draw_rect(label, (0, 0, 255))
+
+    def detect_objects(self):
+        for rect in find_bounding_rects(self.img):
+            if rect.width > CELL_SIZE and rect.height > CELL_SIZE:
+                text = self.detect_text(rect)
+                self.draw_text(text, rect)
+                self.cells.append(Cell(rect, text))
+            else:
+                label = Label(rect)
+                self.labels.append(label)
+                for other in self.labels:
+                    if label.dist(other) < 30:
+                        label.connected.append(other)
+                        other.connected.append(label)
+
+    def process_labels(self):
+        labels_merged = []
+        for label in self.labels:
+            if label.merged:
+                continue
+            label.merge()
+            labels_merged.append(label)
+            label.text = self.detect_text(label.enlarge(10))
+        self.labels = labels_merged
+
+    def process_cells(self):
         for cell in self.cells:
-            if cell.text[0] == "+":
-                term += If(cell.variable, int(cell.text[1:]), 0)
-            else:
-                term *= If(cell.variable, int(cell.text[1:]), 1)
-        return term == int(self.text)
+            self.move_mouse(cell.center_x, cell.center_y)
+            time.sleep(0.5)
 
+            img2 = self.screenshot()
+            img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+            _, img2 = cv2.threshold(img2, 120, 255, cv2.THRESH_BINARY_INV)
+            diff = img2 - self.img
 
-def detect_text(img, rect):
-    x, y, w, h = rect
-    return pytesseract.image_to_string(img[y:y+h, x:x+w], config="tesseract.conf")
+            for cell2 in self.cells:
+                diff[cell2.enlarge(5).to_slice()] = 0
+            for label in self.labels:
+                diff[label.enlarge(3).to_slice()] = 0
 
+            _, diff = cv2.threshold(diff, 110, 255, cv2.THRESH_BINARY)
 
-def findBoundingRects(img, inside=False):
-    contours, _ = cv2.findContours(img, cv2.RETR_LIST if inside else cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    return [cv2.boundingRect(contour) for contour in contours]
+            for rect in find_bounding_rects(diff, inside=True):
+                if rect.width > CELL_SIZE and rect.height > CELL_SIZE:
+                    if cell.square is None:
+                        self.add_square_constraint(rect)
+                elif abs(cell.center_x - rect.center_x) < 5 or abs(cell.center_y - rect.center_y) < 5:
+                    self.add_line_constraint(rect, cell)
 
+    def add_square_constraint(self, rect):
+        square = Constraint(rect)
+        for cell in self.cells:
+            if square.contains(cell):
+                square.cells.append(cell)
+                cell.square = square
+        self.squares.append(square)
 
-def screenshot():
-    with mss.mss() as sct:
-        return np.array(sct.grab(MONITOR))
-
-
-def solve():
-    img = screenshot()
-
-    img_orig = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY_INV)
-
-    cells = []
-    squares = []
-    lines = []
-    labels = []
-
-    for rect in findBoundingRects(img):
-        x, y, w, h = rect
-
-        img_orig = cv2.rectangle(img_orig, rect, (0, 0, 255))
-        if w > CELL_SIZE and h > CELL_SIZE:
-            text = detect_text(img, rect)
-            img_orig = cv2.putText(img_orig, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cells.append(Cell(rect, text))
-        else:
-            label = Label(rect)
-            labels.append(label)
-            for other in labels:
-                if label.dist(other) < 30:
-                    label.connected.append(other)
-                    other.connected.append(label)
-
-    labels_merged = []
-    for label in labels:
-        if label.merged:
-            continue
-        label.merge()
-        labels_merged.append(label)
-        label.text = detect_text(img, label.to_rect(10))
-    labels = labels_merged
-    all_labels = labels[:]
-
-    for cell in cells:
-        win32api.SetCursorPos((cell.center_x + LEFT + BORDER_LEFT, cell.center_y + TOP + BORDER_TOP))
-        time.sleep(0.5)
-
-        img2 = screenshot()
-        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        _, img2 = cv2.threshold(img2, 120, 255, cv2.THRESH_BINARY_INV)
-        diff = img2-img
-
-        for cell2 in cells:
-            diff[cell2.y-5:cell2.y2+5, cell2.x-5:cell2.x2+5] = 0
-        for label in all_labels:
-            diff[label.y-3:label.y2+3, label.x-3:label.x2+3] = 0
-
-        _, diff = cv2.threshold(diff, 110, 255, cv2.THRESH_BINARY)
-
-        for rect in findBoundingRects(diff, inside=True):
-            if rect[2] > CELL_SIZE and rect[3] > CELL_SIZE:
-                if cell.square is None:
-                    square = Square(rect)
-                    for cell2 in cells:
-                        if square.x < cell2.x < square.x2 and square.y < cell2.y < square.y2:
-                            square.cells.append(cell2)
-                            cell2.square = square
-                    squares.append(square)
-            elif abs(cell.center_x - rect[0] - rect[2] // 2) < 5 or abs(cell.center_y - rect[1] - rect[3] // 2) < 5:
-                for line in lines:
-                    if abs(line.x - rect[0]) < 5 and abs(line.y - rect[1]) < 5:
-                        line.cells.append(cell)
-                        break
-                else:
-                    line = Line(rect, cell)
-                    label = min(labels, key=line.dist)
-                    labels.remove(label)
-                    line.text = label.text
-                    lines.append(line)
-                    img_orig = cv2.line(img_orig, (line.center_x, line.center_y),
-                                        (label.center_x, label.center_y), (255, 0, 0))
-                    img_orig = cv2.rectangle(img_orig, label.to_rect(), (255, 0, 0))
-
-    for square in squares:
-        for label in labels:
-            if square.x < label.x < square.x2 and square.y < label.y < square.y2:
-                square.text = label.text
-                labels.remove(label)
-                img_orig = cv2.line(img_orig, (square.x, square.y), (label.center_x, label.center_y), (0, 255, 0))
-                img_orig = cv2.rectangle(img_orig, label.to_rect(), (0, 255, 0))
+    def add_line_constraint(self, rect, cell):
+        for line in self.lines:
+            if rect.dist(line) < 5:
+                line.cells.append(cell)
                 break
-        img_orig = cv2.putText(img_orig, square.text, (square.x, square.y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            line = LineConstraint(rect, cell)
+            self.lines.append(line)
 
-    if labels:
-        for label in labels:
-            img_orig = cv2.rectangle(img_orig, label.to_rect(), (0, 0, 255))
+    def process_lines(self):
+        for line in self.lines:
+            line.sort_cells()
+            label = min(self.labels, key=line.dist)
+            line.text = label.text
+            self.labels.remove(label)
 
-    for line in lines:
-        line.order_cells()
-        x = line.x
-        y = line.y
-        img_orig = cv2.putText(img_orig, line.text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        for cell in line.cells:
-            img_orig = cv2.line(img_orig, (x, y), (cell.center_x, cell.center_y), (0, 0, 255))
-            x = cell.center_x
-            y = cell.center_y
+            self.draw_line((line.center_x, line.center_y), (label.center_x, label.center_y), (255, 0, 0))
+            self.draw_rect(label, (255, 0, 0))
 
-    for cell in cells:
-        if cell.square is not None:
-            img_orig = cv2.line(img_orig, (cell.x, cell.y), (cell.square.x, cell.square.y), (0, 255, 0))
+            x = line.x
+            y = line.y
+            self.draw_text(line.text, line, (0, 0, 255))
+            for cell in line.cells:
+                self.draw_line((x, y), (cell.center_x, cell.center_y), (0, 0, 255))
+                x = cell.center_x
+                y = cell.center_y
 
-    solver = Solver()
+    def process_squares(self):
+        for square in self.squares:
+            for label in self.labels:
+                if square.contains(label):
+                    square.text = label.text
+                    self.labels.remove(label)
+                    self.draw_line((square.x, square.y), (label.center_x, label.center_y), (0, 255, 0))
+                    self.draw_rect(label, (0, 255, 0))
+                    break
+            self.draw_text(square.text, square, (0, 255, 0))
+            for cell in square.cells:
+                self.draw_line((cell.x, cell.y), (cell.square.x, cell.square.y), (0, 255, 0))
 
-    for line in lines:
-        solver.add(simplify(line.get_constraint()))
+    def solve_level(self):
+        solver = Solver()
 
-    for square in squares:
-        solver.add(simplify(square.get_constraint()))
+        for line in self.lines:
+            solver.add(simplify(line.get_constraint()))
 
-    if solver.check() == sat:
-        model = solver.model()
-        for cell in cells:
-            x = cell.center_x + LEFT + BORDER_LEFT
-            y = cell.center_y + TOP + BORDER_TOP
-            win32api.SetCursorPos((x, y))
-            time.sleep(0.01)
-            if model[cell.variable]:
-                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
-                time.sleep(0.01)
-                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
-            else:
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-                time.sleep(0.01)
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-            time.sleep(0.01)
-        return True
+        for square in self.squares:
+            solver.add(simplify(square.get_constraint()))
 
-    print(solver)
-    cv2.imshow("Detection", img_orig)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return False
+        if solver.check() == sat:
+            model = solver.model()
+            for cell in self.cells:
+                self.click(cell.center_x, cell.center_y, model[cell.variable])
+        else:
+            print("Solver failed")
+            print(solver)
+            cv2.imshow("Detection", self.img_orig)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
-time.sleep(5)
+def main():
+    time.sleep(5)
+    solver = CrossCellsSolver()
 
-for _ in range(5):
-    solve()
-    win32api.SetCursorPos((TOP, LEFT))
-    time.sleep(7)
+    for _ in range(5):
+        solver.do_level()
+        solver.move_mouse()
+        time.sleep(7)
+
+
+if __name__ == "__main__":
+    main()
